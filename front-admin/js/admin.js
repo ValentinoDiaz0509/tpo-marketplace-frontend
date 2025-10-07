cdc16425dac238de0fce9cdce1ebe2b3993516b2
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-10-07 17:00:12 -0300

    Delete front-admin directory

--- front-admin/js/admin.js
+++
@@ -1,5 +0,0 @@
-const token = localStorage.getItem("token");
-if (!token) {
-  alert("Acceso denegado. Iniciá sesión como administrador.");
-  window.location.href = "../front-usuario/login.html";
-}

c963a7caef70ce38f20c4a5a052042a1e453e08d
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-10-17 14:04:49 -0300

    2 funciones para wishlist

--- marketplace-frontend/src/utils/api.js
+++ marketplace-frontend/src/utils/api.js
@@ -1,12 +1,29 @@
-export const API_URL = "http://localhost:4002";
+export const API_URL = "http://localhost:4002"; 
 
 export async function fetchData(endpoint, options = {}) {
   const token = localStorage.getItem("token");
   const headers = {
     "Content-Type": "application/json",
-    ...(token && { "Authorization": `Bearer ${token}` })
+    ...(token && { Authorization: `Bearer ${token}` }),
   };
   const res = await fetch(`${API_URL}${endpoint}`, { ...options, headers });
-  if (!res.ok) throw new Error(`Error ${res.status}`);
+  if (!res.ok) {
+    const errorBody = await res.json().catch(() => ({ message: `Error ${res.status}` }));
+    throw new Error(errorBody.message);
+  }
   return res.json();
 }
+
+
+
+export const fetchWishlistAPI = () => {
+  return fetchData("/api/v1/wishlist");
+};
+
+
+export const addToWishlistAPI = (gameId) => {
+  return fetchData("/api/v1/wishlist", {
+    method: 'POST',
+    body: JSON.stringify({ gameId: gameId }),
+  });
+};

0a620599aca9d09565dfcfce964bb2345b5fed61
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-10-18 16:08:43 -0300

    Update vite.config.js

--- marketplace-frontend/vite.config.js
+++ marketplace-frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"; // <--- Este es el importante
 
 // https://vite.dev/config/
 export default defineConfig({

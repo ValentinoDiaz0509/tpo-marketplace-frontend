1da4f1eb0e37a703eeb40b5d7ef1774a37d63539
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-10-18 16:08:55 -0300

    Update postcss.config.cjs

--- marketplace-frontend/postcss.config.cjs
+++ marketplace-frontend/postcss.config.cjs
@@ -1,9 +1,1 @@
-module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {},
 
-    // 2. Autoprefixer debe ir después para añadir prefijos de navegadores
-    //    al CSS generado por Tailwind.
-    autoprefixer: {},
-  },
-};

dc7bb05de12b3afc23e25ee865d45dd3aba0000f
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-10-21 00:28:02 -0300

    Update App.jsx

--- marketplace-frontend/src/App.jsx
+++ marketplace-frontend/src/App.jsx
@@ -1,10 +1,26 @@
-import AppRouter from "../src/router/AppRouter";
-import "../src/App.css";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; // Importa los estilos
+import AppRouter from './router/AppRouter';
+import { AuthProvider } from './context/AuthContext';
 
-export default function App() {
+function App() {
   return (
-    <>
+    <AuthProvider>
       <AppRouter />
-    </>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </AuthProvider>
   );
 }
+
+export default App;

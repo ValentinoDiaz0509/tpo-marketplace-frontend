bd33767ea0b57f59852083ef9cefd645aba772a6
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-11-23 16:37:29 -0300

    Update AdminLayout.jsx

--- marketplace-frontend/src/components/layout/AdminLayout.jsx
+++ marketplace-frontend/src/components/layout/AdminLayout.jsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import AdminNavbar from "./AdminNavbar";
 
 const AdminLayout = () => {
-  return (
-    <div>
-      <AdminNavbar />
-      <Outlet />
-    </div>
-  );
+  return (
+    <div className="flex flex-col min-h-screen bg-[#121212] text-white">
+      <AdminNavbar />
+      <div className="flex-grow">
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default AdminLayout;
